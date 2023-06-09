050678ccc2a36971f3cfc43f14098eef2c4de8db
Cheenue Thao <cheenuethao@gmail.com>
2023-06-08 21:31:34 -0500

    week 14 coding assignment

--- src/main/java/com/petstore/service/PetStoreService.java
+++ src/main/java/com/petstore/service/PetStoreService.java
@@ -1,9 +1,19 @@
 package com.petstore.service;
 
+import com.petstore.controller.model.PetStoreData;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class PetStoreService {
 
+    public PetStoreData savePetStore(PetStoreData petStoreData) {
+
+        findOrCreatePetStoreId(petStoreData.getPetStoreId());
+        return null;
+    }
+
+    private void findOrCreatePetStoreId(Long petStoreId) {
+        return null;
+    }
 }
